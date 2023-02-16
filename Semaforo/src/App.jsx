9de85355224630d69022beb9fdcda07ac104026d
Marcelo Costa <marcelo.cas94@gmail.com>
2023-02-16 17:15:13 -0300

    Criação de formas do semáforo

--- Semaforo/src/App.jsx
+++ Semaforo/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+  const [cor, setCor] = useState("")
   return (
     <div className="App">
-      
+      <div className="sinalizacao">
+        <div className="sinalizacao__luz sinalizacao__luz--ligado" style={{ backgroundColor: 'red' }} />
+        <div className="sinalizacao__luz" style={{ backgroundColor: 'yellow' }} />
+        <div className="sinalizacao__luz" style={{ backgroundColor: 'lime' }} />
+      </div>
     </div>
   )
 }
