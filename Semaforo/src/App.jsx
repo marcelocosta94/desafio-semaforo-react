f1b313c9f9fe200e8d2d7cd75b394870352e100f
Marcelo Costa <marcelo.cas94@gmail.com>
2023-02-23 11:47:25 -0300

    Primeiras alterações para acender o semáforo

--- Semaforo/src/App.jsx
+++ Semaforo/src/App.jsx
@@ -3,12 +3,13 @@ import './App.css'
 
 function App() {
   const [cor, setCor] = useState("")
+  console.log(cor)
   return (
     <div className="App">
       <div className="sinalizacao">
-        <div className="sinalizacao__luz sinalizacao__luz--ligado" style={{ backgroundColor: 'red' }} />
-        <div className="sinalizacao__luz" style={{ backgroundColor: 'yellow' }} />
-        <div className="sinalizacao__luz" style={{ backgroundColor: 'lime' }} />
+        <div onClick={() => setCor('red')}className={`sinalizacao__luz ${cor === 'red' ? "sinalizacao__luz--ligado" : ""}`} style={{ backgroundColor: 'red' }} />
+        <div onClick={() => setCor('yellow')}className={`sinalizacao__luz ${cor === 'yellow' ? "sinalizacao__luz--ligado" : ""}`} style={{ backgroundColor: 'yellow' }} />
+        <div onClick={() => setCor('green')}className={`sinalizacao__luz ${cor === 'green' ? "sinalizacao__luz--ligado" : ""}`} style={{ backgroundColor: 'lime' }} />
       </div>
     </div>
   )
